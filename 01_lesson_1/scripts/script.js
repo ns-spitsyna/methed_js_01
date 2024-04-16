9c1d93fe301e6e8d28482152f7fc0f47bbe76bf0
ns-spitsyna <spitcinan@gmail.com>
2024-04-16 23:05:16 +0700

    Модуль 1 Урок 2

--- 01_lesson_1/scripts/script.js
+++ 01_lesson_1/scripts/script.js
@@ -1,8 +1,23 @@
 'use strict';
+{
+    let productName = "Мыло",
+        productAmount = 5,
+        productСategory = "Гигиена",
+        productPrice = 30,
+        sum;
 
-let productName = "Мыло",
-    productAmount = 5,
-    productСategory = "Гигиена",
-    productPrice = 30;
+    sum = productAmount * productPrice;
+    console.log(productName);
+    console.log(`Общая сумма товара: ${sum}`);
+}
+{
+    let productName = "Кружка",
+        productAmount = 10,
+        productСategory = "Посуда",
+        productPrice = 50,
+        sum;
 
-console.log(productName);
+    sum = productAmount * productPrice;
+    console.log(productName);
+    console.log(`Общая сумма товара: ${sum}`);
+}
